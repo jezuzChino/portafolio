0044cdbc29d57b8d98697473295335476648cccc
jezuzChino <jesus.chino.r@gmail.com>
2018-09-22 18:36:18 -0500

    import del modulo routing dentro del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ItemComponent } from './pages/item/item.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
+  declarations: [ // En un modulo las declaraciones son solo para los pipes, componentes, etc
     AppComponent,
     HeaderComponent,
     FooterComponent,
@@ -17,8 +18,9 @@ import { ItemComponent } from './pages/item/item.component';
     AboutComponent,
     ItemComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [ // Aqui se colocan los modulos
+    BrowserModule,
+    AppRoutingModule // se importa el modulo creado asi como la ruta en la linea 10
   ],
   providers: [],
   bootstrap: [AppComponent]
