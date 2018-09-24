020d3ccf51726427c9f5d378df67de8d2c8e5c73
jezuzChino <jesus.chino.r@gmail.com>
2018-09-23 21:59:36 -0500

    import y uso de la interface

--- src/app/services/info-page.service.ts
+++ src/app/services/info-page.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPageService {
 
-  info: any = { };
+  info: InfoPagina = { };
   cargada = false;
 
   constructor( private http: HttpClient) {
@@ -14,7 +15,7 @@ export class InfoPageService {
     console.log('Servicio listo');
     // Leer archivo JSON data-page-json
     this.http.get('assets/data/data-page.json')
-    .subscribe( resp => {
+    .subscribe( (resp: InfoPagina) => {
 
       this.cargada = true;
       this.info = resp;
