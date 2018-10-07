d2694d38f92bebdf02a522c309f4b3e709328aa1
jezuzChino <jesus.chino.r@gmail.com>
2018-10-07 17:01:05 -0500

    import data in header componet

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoPageService } from '../../services/info-page.service';
 
 @Component({
   selector: 'app-header',
@@ -6,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+// nombre: tipo
+  constructor( public infoPageService: InfoPageService ) { }
 
   ngOnInit() {
   }
