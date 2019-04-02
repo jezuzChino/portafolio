f17931e330e778ca09ce5a8da3077555df17d4c8
Jesús Chino <jesus.chino.r@gmail.com>
2019-04-02 17:52:05 -0600

    Se consume servicio de firebase para llenar la información del "about"

--- src/app/services/info-page.service.ts
+++ src/app/services/info-page.service.ts
@@ -9,19 +9,34 @@ export class InfoPageService {
 
   info: InfoPagina = { };
   cargada = false;
+  equipo: any [] = [];
 
   constructor( private http: HttpClient) {
+      this.cargarinfo();
+      this.cargarEquipo();
+   }
 
-    console.log('Servicio listo');
-    // Leer archivo JSON data-page-json
-    this.http.get('assets/data/data-page.json')
-    .subscribe( (resp: InfoPagina) => {
+   private cargarinfo() {
+      console.log('Servicio listo');
+      // Leer archivo JSON data-page-json
+      this.http.get('assets/data/data-page.json')
+      .subscribe( (resp: InfoPagina) => {
 
-      this.cargada = true;
-      this.info = resp;
-      console.log( resp );
-      // console.log(resp.nombre_corto);
-      // console.log( resp['nombre_corto']);
-    });
+        this.cargada = true;
+        this.info = resp;
+        console.log( resp );
+        // console.log(resp.nombre_corto);
+        // console.log( resp['nombre_corto']);
+      });
+   }
+
+   private cargarEquipo() {
+
+      this.http.get('https://angular-portafolio-2faeb.firebaseio.com/equipo.json')
+      .subscribe( (resp: any[]) => {
+
+        this.equipo = resp;
+        console.log( resp );
+      });
    }
 }
